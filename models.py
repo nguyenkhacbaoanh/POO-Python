aad7c3ddc1e3e0f7033064423827d3ceec0b801d
bao anh <khacbaoanh.nguyen@gmail.com>
2017-07-08 22:44:24 +0200

    set all attributes from JSON

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import json
+
 class Agent:
 
     def __init__(self, agent_attrs):
@@ -7,7 +9,18 @@ class Agent:
             setattr(self, attr_name, attr_value)
 
 
-agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943,
-                    "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": False, "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
-agent = Agent(agent_attributes)
-print(agent.agreeableness)
+#agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943,
+                    #"date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": False, "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+
+def read_json(source):
+    source = str(source)
+    with open(source) as f:
+        data = json.load(f)
+    return data
+
+def main():
+    for agent_attributes in read_json('agents-100k.json'):
+        agent = Agent(agent_attributes)
+        print(agent.age)
+
+main()
