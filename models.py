631b3710ef66940c21c9af804d0f4b0159451090
supig <khacbaoanh.nguyen@gmail.com>
2017-07-09 08:11:10 +0200

    create Position class

--- models.py
+++ models.py
@@ -1,16 +1,29 @@
 import json
+import math
 
 class Agent:
 
-    def __init__(self, **agent_attrs):
+    def __init__(self, position, **agent_attrs):
+        #add a parameter position in class Agent
+        self.position = position
         # a loop to set all attributes from JSON to class
         for attr_name, attr_value in agent_attrs.items():
             # set attribute
             setattr(self, attr_name, attr_value)
 
+#Create new class Position
+class Position:
+    
+    #initialization
+    def __init__(self, latitude_degrees, longitude_degrees):
+        self.latitude_degrees = latitude_degrees
+        self.longitude_degrees = longitude_degrees
 
-#agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943,
-                    #"date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": False, "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+    #to convert from latitude degree to radian
+    #use method property to set this method like attribute, it means that this method don't need ()
+    @property
+    def latitude(self):
+        return (self.latitude_degrees * math.pi / 180)
 
 def read_json(source):
     source = str(source)
@@ -21,7 +34,12 @@ def read_json(source):
 def main():
     #a loop to set attributes from list of 100k agents
     for agent_attributes in read_json('agents-100k.json'):
-        agent = Agent(**agent_attributes)
-        print(agent.age)
+        # use method pop in dist to make instance and delete this key:value in dist
+        #because we don't need anymore
+        latitude = agent_attributes.pop('latitude')
+        longitude = agent_attributes.pop('longitude')
+        position = Position(latitude, longitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.latitude)
 
 main()
